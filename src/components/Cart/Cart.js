f9b807144d017abf95c44528caf4e0fb99dfcdce
Sakib Bin Ehsan <sakibehsan13@gmail.com>
2020-10-31 01:14:46 +0600

    Shop Page Coding Done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart);
+    //const totalPrice = cart.reduce((total, pd) => total + pd.price, 0)
+    let totalPrice = 0;
+    for(let i=0;i<cart.length;i++) {
+        const product = cart[i];
+        totalPrice = totalPrice + product.price;
+    }
+
+    let shippingCost = 0;
+    if(totalPrice > 35) {
+        shippingCost = 0;
+    }
+    else if(totalPrice > 15) {
+        shippingCost = 4.99;
+    }
+    else if(totalPrice > 0) {
+        shippingCost = 12.99;
+    }
+
+    const tax = totalPrice / 10;
+    const grandTotal = (totalPrice + shippingCost + Number(tax)).toFixed(2);
+
+    const formatNumber = (num) => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items ordered: {cart.length}</p>
+            <p>Product Price: {formatNumber(totalPrice)}</p>
+            <p><small>Shipping Cost: {shippingCost}</small></p>
+            <p><small>Tax + VAT: {formatNumber(tax)}</small></p>
+            <p>Total Price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
